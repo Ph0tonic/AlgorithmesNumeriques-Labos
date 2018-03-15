91af7be3a71ae51ae037b696138369f6dbc9c93f
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-03-15 20:57:09 +0100

    Add tangente methode + fixes zoom reset & nb functions changing

--- Labo2/script.js
+++ Labo2/script.js
@@ -3,6 +3,12 @@ class Plot{
   constructor(divId){
     this.divId = divId;
     this.function = document.getElementById('eq').value;
+    this.options = {
+      target: '#'+this.divId,
+      width: 800,
+      height: 500,
+      data: []
+    };
   }
 
   addAlgorithme(algorithme){
@@ -20,19 +26,29 @@ class Plot{
     }
 
     this.function = document.getElementById('eq').value;
-    let graph = [{
+    this.options.data[0] = {
       fn: this.function,
       sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
       graphType: 'polyline'
-    }];
+    };
 
-    console.log("data to log: ",this.algorithme.getData());
-    let data = graph.concat(this.algorithme.draw());
-    console.log(data);
-    functionPlot({
-      target: '#'+this.divId,
-      data: data
-    });
+    let functions = this.algorithme.draw();
+    for(let i=1;i<Math.max(this.options.data.length,functions.length+1);i++){
+        if(functions.length>i-1){
+          this.options.data[i] = functions[i-1];
+        }else if(this.options.data[i].fnType === 'points'){   // Workaround because of some problem in librairie when number of functions has changed when updating
+          this.options.data[i].points = [];
+        }else if(this.options.data[i].fnType === 'vector'){
+          this.options.data[i].vector = [];
+        }else{
+          this.options.data[i].fn = "";
+        }
+    }
+    functionPlot(this.options);
+  }
+
+  getFunction(){
+    return this.function;
   }
 
   getValue(x){
@@ -44,12 +60,12 @@ class Plot{
 
   getX1(){
     //TODO add parametrized starting point
-    return -10;
+    return -7;
   }
 
   getX2(){
     //TODO add parametrized second point -> for Dichotomie only
-    return 4;
+    return -1;
   }
 
   addLine(data){
@@ -109,7 +125,7 @@ class Dichotomie extends Algorithme{
     this.x1 = this.plot.getX1();
     this.x2 = this.plot.getX2();
     this.zeroFounded = false;
-    if(Dichotomie._sameSign(this.x1, this.x2)){
+    if(this._evaluateBorneSigneEqual()){
       console.log("Erreur, bornes du même signes");
     }
   }
@@ -118,7 +134,7 @@ class Dichotomie extends Algorithme{
     this.step++;
     if(this.step > this.data.length && !this.zeroFounded){
       //Calcul de la prochaine étape et ajout de celle-ci dans le tableau data
-      if(Dichotomie._sameSign(this.plot.getValue(this.x1), this.plot.getValue(this.x2))){
+      if(this._evaluateBorneSigneEqual()){
         console.log("Erreur, bornes du même signes");
       }else{
         let middle = (this.x1 + this.x2)/2;
@@ -145,7 +161,6 @@ class Dichotomie extends Algorithme{
       //Requirement of function-plot, mimimum 2 couple of values
       data.push(data[0]);
     }
-    console.log(data);
     return [{
         points: data,
         attr:{'stroke-width': 3},
@@ -153,7 +168,10 @@ class Dichotomie extends Algorithme{
         color: 'red',
         graphType: 'scatter'
     }];
+  }
 
+  _evaluateBorneSigneEqual(){
+    return Dichotomie._sameSign(this.plot.getValue(this.x1), this.plot.getValue(this.x2));
   }
 
   static _sameSign(x1, x2){
@@ -164,11 +182,67 @@ class Dichotomie extends Algorithme{
 class Tangente extends Algorithme{
   constructor(plot){
     super(plot);
+    this.x1 = this.plot.getX1();
+    this.zeroFounded = false;
+    try {
+      this._derivative(this.x1);
+    }catch(err){
+      console.log("Erreur, dérivée de la fonction == 0");
+    }
   }
 
   nextStep(){
-    //Pourra être utile: math.derivative('x^2', 'x').eval({x: 4});
+    this.step++;
+    if(this.step > this.data.length && !this.zeroFounded){
+      //Calcul de la prochaine étape et ajout de celle-ci dans le tableau data
+      try {
+        //Calcul du nouveau x -> ax+b=0 avec a=derivative et b=y(x)
+        let a = this._derivative(this.x1);
+        let y = this.plot.getValue(this.x1);
+        let b = y - a*this.x1;
+        let x = -b/a;
+        let func = a+' * x + '+ b;
+
+        this.data.push([func,this.x1,y]);
+        this.x1 = x;
+
+        //TODO update to test with Epsilon
+        if(this.plot.getValue(this.x1) == 0){
+          this.zeroFounded = true;
+        }
+      }catch(err){
+        console.log("Erreur, dérivée de la fonction = 0");
+      }
+    }
   }
+
+  draw(){
+    let data = this.getData();
+    let graph = [];
+    data.forEach(function(element) {
+      graph.push({
+        fn: element[0],
+        sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
+        graphType: 'polyline',
+        color: 'red'
+      },{
+        vector: [0, element[2]],
+        offset: [element[1], 0],
+        graphType: 'polyline',
+        fnType: 'vector'
+      });
+    });
+    return graph;
+  }
+
+  _derivative(x){
+    let value = math.derivative(this.plot.getFunction(), 'x').eval({x: x});
+    if(value===0){
+      throw new Error("Derivative = 0");
+    }
+    return value;
+  }
+
 }
 
 class PointFixe extends Algorithme{
@@ -177,7 +251,11 @@ class PointFixe extends Algorithme{
   }
 
   nextStep(){
+    //TODO
+  }
 
+  draw(){
+    //TODO
   }
 }
 
@@ -213,9 +291,10 @@ class PointFixe extends Algorithme{
   }
 }*/
 
-let plot = new Plot('canvas',);
-let dichotomie = new Dichotomie(plot);
-plot.addAlgorithme(dichotomie);
+let plot = new Plot('canvas');
+//let methode = new Dichotomie(plot);
+let methode = new Tangente(plot);
+plot.addAlgorithme(methode);
 
 $(document).ready(function(){
   $('#form').on('submit', function (event) {
