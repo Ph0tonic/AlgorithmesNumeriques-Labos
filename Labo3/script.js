8750661ad6058d05e6299bbddfcde0df5dc0f44d
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-04-04 17:17:55 +0200

    Display results + design

--- Labo3/script.js
+++ Labo3/script.js
@@ -13,7 +13,6 @@ class Matrix {
   }
 
   _load(d){
-    console.log(d);
     d = JSON.parse(d);
 
     this.size = d.n[0]; //Get the size of the matrix
@@ -54,7 +53,6 @@ class Matrix {
       this.data[iRowSub][i] -= this.data[iRowRef][i] * factor;
     }
   }
-
 }
 
 class Solver {
@@ -68,29 +66,47 @@ class Solver {
     this.x = [];
     this.x.length = matrix.size;
 
-    this._startTimer();
+    let start;
+    let stop;
 
-    this.gaussTransform(matrix);
-    this.resolveX(matrix,this.x);
+    //Start counter
+    try{
+      start = performance.now();
 
-    this._stopTimer();
+      this.gaussTransform(matrix); //Gauss transformation to upper diagonal matrix
+      this.resolveX(matrix,this.x); //Resolve upper diagonal matrix with substitution method
 
-    //Affichage des résultats
+      //Stop counter
+      stop = performance.now();
+
+      //Display time needed
+      document.getElementById("chronotime").innerHTML = (stop - start) + "ms";
+
+    }catch(error){
+      this.x = undefined;
+    }
+
+    //Display x
+    this.displayX();
   }
 
   gaussTransform(matrix){
+    //Cache variable to improve speed
+    let data = matrix.data;
+
     let m = matrix.size;
     if(m===0){
       return;
     }
-    let n = matrix.data[0].length;
+    let n = data[0].length;
 
 
     let h = 0; /* initialization of the pivot row */
     let k = 0; /* initialization of the pivot column */
 
-    /*Recherche de la plus grande valeur, raison:
-        "In any case, choosing the largest possible absolute value of the pivot improves the numerical stability of the algorithm, when floating point is used for representing numbers."
+    /*
+        Search for the greatest value, reason:
+        - "In any case, choosing the largest possible absolute value of the pivot improves the numerical stability of the algorithm, when floating point is used for representing numbers."
         Sources : Wikipedia -> https://en.wikipedia.org/wiki/Gaussian_elimination#Pseudocode
     */
 
@@ -99,9 +115,11 @@ class Solver {
     while(h < m) { //  && k < n){
       /* Find the k-th pivot: */
       iMax = matrix.indiceMaxColumn(k,h); // k -> column && h -> line start searching
-      if(matrix.data[iMax][k] == 0) {
+      if(data[iMax][k] == 0) {
         /* No pivot in this column, pass to next column */
         ++k;
+        console.log("No constant solutions");
+        throw new Error("Résultat non-constant");
       } else {
         if(h != iMax){
           matrix.swapRows(h, iMax); // h -> current row && iMax -> ligne max
@@ -110,10 +128,10 @@ class Solver {
         /* Do for all rows below pivot: */
         for(let i=h+1; i<m; ++i){
           //Calcul the factor
-          f = matrix.data[i][k] / matrix.data[h][k];
+          f = data[i][k] / data[h][k];
 
           /* Fill with zeros the lower part of pivot column: */
-          matrix.data[i][k] = 0;
+          data[i][k] = 0;
 
           /* Do for all remaining elements in current row: */
           matrix.substractLineFactor(h, i, f, k+1);
@@ -128,50 +146,96 @@ class Solver {
 
   resolveX(matrix){
     let n = matrix.size;
+    let data = matrix.data;
     for (let i=n-1; i >= 0; --i) {
-        this.x[i] = matrix.data[i][n]/matrix.data[i][i];
+        this.x[i] = data[i][n]/data[i][i];
         for (let k=i-1; k > -1; k--) {
-            matrix.data[k][n] -= matrix.data[k][i] * this.x[i];
+            data[k][n] -= data[k][i] * this.x[i];
         }
     }
   }
 
-  _startTimer(){
-    this.start = new Date();
-  }
+  displayX(){
+    let result = document.getElementById('result');
+    let htmlResult = "";
 
-  _stopTimer(){
-    this.stop = new Date();
-    document.getElementById("chronotime").innerHTML = (this.stop - this.start) + "ms";
+    if(this.x != undefined){
+      if(this.x.length > 0){
+        for(let i=0;i<this.x.length;i++){
+          htmlResult += "x<sub>"+(i+1)+"</sub> = "+this.x[i]+"<br>";
+        }
+      }else{
+        htmlResult = '<div class="alert alert-warning">Matrice de taille null!</div>';
+      }
+    }else{
+      htmlResult = '<div class="alert alert-danger">Aucune solution constante trouvée!</div>';
+    }
+    result.innerHTML = htmlResult;
   }
-
 }
 
-// modified from http://html5demos.com/file-api
-let holder = document.getElementById('holder');
 let matrix = new Matrix();
-let s = new Solver();
 
-holder.ondragover = function() {
-    this.className = 'hover';
-    return false;
-};
-holder.ondragend = function() {
-    this.className = '';
-    return false;
-};
-holder.ondrop = function(e) {
-    this.className = '';
-    e.preventDefault();
+function loadClickedFile(e) {
+    e = e || window.event;
+    let file = e.target || e.srcElement;
+    file = './data/'+file.innerHTML;
+    let xmlhttp = new XMLHttpRequest();
 
-    let file = e.dataTransfer.files[0]; //Open the first file
-    let reader = new FileReader();
-    reader.onload = function(event) {
-        matrix = new Matrix(event.target.result);
-        holder.innerHTML = "LOADED !!!";
+    xmlhttp.overrideMimeType('text/json');
+    xmlhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200)
+      {
+        holder.value = this.responseText;
+        matrix = new Matrix(this.responseText);
+      }
     };
-    console.log(file);
-    reader.readAsText(file);
+    xmlhttp.open("GET", file, true);
+    xmlhttp.send(undefined);
+}
 
-    return false;
+document.onreadystatechange = () => {
+  if (document.readyState === 'interactive') {
+    // document ready
+
+    //To avoid error with chrome and loading file with xmlhttprequest
+    let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if(!isChrome){
+      let ul = document.getElementById('files');
+      ul.onclick = function(event) {
+          loadClickedFile(event);
+      };
+    }
+
+    // modified from http://html5demos.com/file-api
+    let holder = document.getElementById('holder');
+    let s = new Solver();
+
+    document.getElementById('solve').onclick = function(){
+        matrix = new Matrix(holder.value);
+        s.solve(matrix);
+    };
+
+    holder.ondragover = function() {
+        this.className = 'hover';
+        return false;
+    };
+    holder.ondragend = function() {
+        this.className = '';
+        return false;
+    };
+    holder.ondrop = function(e) {
+        e.preventDefault();
+
+        let file = e.dataTransfer.files[0]; //Open the first file
+        let reader = new FileReader();
+        reader.onload = function(event) {
+            holder.value = event.target.result;
+            matrix = new Matrix(event.target.result);
+        };
+
+        reader.readAsText(file);
+        return false;
+    };
+  }
 };
