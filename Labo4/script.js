ffbb3444a81266b096daf04619669a14e43d187c
Raphaël Margueron <raphael.margueron@gmail.com>
2018-04-12 20:34:41 +0200

    adding the controls

--- Labo4/script.js
+++ Labo4/script.js
@@ -20,19 +20,19 @@ function taylorCos(theta, n)
     return res;
 }
 
-function getPointsTaylorCos(start, stop, count, n)
+function getPointsTaylorCos(start, stop, nbSample, nbTermsTaylor)
 {
-    if(stop <= start || count < 1)
+    if(stop <= start || nbSample < 1)
         return false;
 
     let range = stop - start;
-    let step = range / (count - 1);
+    let step = range / nbSample;
 
     let points = []
 
-    for(let x = start; x <= stop; x+=step)
+    for(let x = start; x < stop; x+=step)
     {
-        y = taylorCos(x, n);
+        y = taylorCos(x, nbTermsTaylor);
         point = [x, y];
         points.push(point);
     }
@@ -40,26 +40,38 @@ function getPointsTaylorCos(start, stop, count, n)
     return points;
 }
 
-function showGraph(graph, points, points2)
+function showGraph(graph, points, points2, points3)
 {
     functionPlot({
         target: '#' + graph,
+        xAxis :{label:'x'},
+        yAxis :{label:'y'},
+        grid :true,
         data: [
-        {
-            points: points,
-            points2: points2,
-            fnType: 'points',
-            graphType: 'polyline'
-        }
+            {
+                points: points,
+                fnType: 'points',
+                graphType: 'polyline'
+            },
+            {
+                points: points2,
+                fnType: 'points',
+                graphType: 'polyline'
+            },
+            {
+                points: points3,
+                fnType: 'points',
+                graphType: 'polyline'
+            }
         ]
     })
 }
 
 function createPeriods(points, nbPeriods, period)
 {
-    newpoints = points.slice(0);
+    let newpoints = clone2DArray(points);
 
-    for(let i = 1; i <= nbPeriods; i++)
+    for(let i = 1; i < nbPeriods; i++)
     {
         for(let j = 0; j < points.length; j++)
         {
@@ -76,18 +88,58 @@ function createPeriods(points, nbPeriods, period)
     return newpoints;
 }
 
-function use()
+function shiftBy(points, value)
 {
-    period = 2*Math.PI;
-    nbPeriods = 10;
-    nbTermsTaylor = 500;
-    nbSteps = 20;
+    let newpoints = clone2DArray(points);
 
-    points = getPointsTaylorCos(-period/2, period/2, nbSteps, nbTermsTaylor);
-    console.log(points);
-    points = createPeriods(points, nbPeriods, period);
-    console.log(points);
-    showGraph('cos', points, points);
+    for(let i = 0; i < newpoints.length; i++)
+    {
+        newpoints[i][0] += value;
+    }
+
+    return newpoints;
 }
 
-use();
+function use(nbTermsTaylor, nbSample, nbPeriods)
+{
+    let period = 2*Math.PI;
+
+    let pointsCos = getPointsTaylorCos(-period/2, period/2, nbSample, nbTermsTaylor);
+    let pointsMSin = shiftBy(pointsCos, -period/4);
+    let pointsMCos = shiftBy(pointsMSin, -period/4);
+
+    pointsCos = createPeriods(pointsCos, nbPeriods, period);
+    pointsMSin = createPeriods(pointsMSin, nbPeriods, period);
+    pointsMCos = createPeriods(pointsMCos, nbPeriods, period);
+
+    showGraph('cos', pointsCos, pointsMSin, pointsMCos);
+}
+
+function clone2DArray(from)
+{
+    to = [];
+    for (var i = 0; i < from.length; i++)
+    {
+        to.push(from[i].slice(0));
+    }
+    return to;
+}
+
+function settingsChanged()
+{
+    let nbTermsTaylor = document.getElementById("nbTermsTaylor").value;
+    let nbSample = document.getElementById("nbSample").value;
+    let nbPeriods = document.getElementById("nbPeriods").value;
+
+    updateDisplay(nbTermsTaylor, nbSample, nbPeriods);
+    use(nbTermsTaylor, nbSample, nbPeriods);
+}
+
+function updateDisplay(nbTermsTaylor, nbSample, nbPeriods)
+{
+    document.getElementById("nbTermsTaylor-value").innerHTML = nbTermsTaylor;
+    document.getElementById("nbSample-value").innerHTML = nbSample;
+    document.getElementById("nbPeriods-value").innerHTML = nbPeriods;
+}
+
+settingsChanged();
